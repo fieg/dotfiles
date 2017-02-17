1abaca26b608264af52d1df7e3b498fb2c890087
fieg <jeroen@fieg.nl>
2017-02-17 14:00:25 +0100

    gcloud files

---
+++ formulas/gcloud/completion.zsh
@@ -0,0 +1,4 @@
+# The next line enables shell command completion for gcloud.
+if [ -f /usr/local/lib/gcloud/google-cloud-sdk/completion.zsh.inc ]; then
+  source '/usr/local/lib/gcloud/google-cloud-sdk/completion.zsh.inc'
+fi
---
+++ formulas/gcloud/path.zsh
@@ -0,0 +1,4 @@
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f /usr/local/lib/gcloud/google-cloud-sdk/path.zsh.inc ]; then
+  source '/usr/local/lib/gcloud/google-cloud-sdk/path.zsh.inc'
+fi
