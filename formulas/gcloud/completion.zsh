c6d67b3564fbea3d9e5c6935ede449069e5f32d5
fieg <jeroen@fieg.nl>
2017-02-17 14:07:03 +0100

    improved gcloud

--- formulas/gcloud/completion.zsh
+++ formulas/gcloud/completion.zsh
@@ -1,4 +1,9 @@
 # The next line enables shell command completion for gcloud.
+if [ -f /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc ]; then
+  source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc'
+fi
+
+# Deprecated
 if [ -f /usr/local/lib/gcloud/google-cloud-sdk/completion.zsh.inc ]; then
   source '/usr/local/lib/gcloud/google-cloud-sdk/completion.zsh.inc'
 fi
--- formulas/gcloud/path.zsh
+++ formulas/gcloud/path.zsh
@@ -1,4 +1,9 @@
 # The next line updates PATH for the Google Cloud SDK.
+if [ -f /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.in ]; then
+  source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.in'
+fi
+
+# Deprecated
 if [ -f /usr/local/lib/gcloud/google-cloud-sdk/path.zsh.inc ]; then
   source '/usr/local/lib/gcloud/google-cloud-sdk/path.zsh.inc'
 fi
