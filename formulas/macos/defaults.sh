a399e688fb8971c3e44bd2416850b4eebde9795d
fieg <jeroen@fieg.nl>
2017-02-20 09:45:15 +0100

    shorten standbydelay from 24 to 3 hours

--- formulas/macos/defaults.sh
+++ formulas/macos/defaults.sh
@@ -22,8 +22,8 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 #sudo scutil --set LocalHostName "mbp"
 #sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "mbp"
 
-# Set standby delay to 24 hours (default is 1 hour)
-sudo pmset -a standbydelay 86400
+# Set standby delay to 3 hours (default is 1 hour)
+sudo pmset -a standbydelay 10800
 
 # Disable the sound effects on boot
 sudo nvram SystemAudioVolume=" "
