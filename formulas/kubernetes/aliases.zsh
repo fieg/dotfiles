254ec347d2bbdda0e832ff352a90890b7b78c739
fieg <jeroen@fieg.nl>
2017-03-03 14:22:47 +0100

    added aliases and completion for kubernetes

---
+++ formulas/kubernetes/aliases.zsh
@@ -0,0 +1,10 @@
+alias k='kubectl'
+
+# Pods
+alias kgp='kubectl get pod'
+alias kdp='kubectl describe pod'
+alias kl='kubectl logs'
+
+# Services
+alias kgs='kubectl get svc'
+alias kds='kubectl describe svc'
