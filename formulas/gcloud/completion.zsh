bda7690dff045648480b894c32a0286827635a23
fieg <jeroen@fieg.nl>
2018-05-17 11:44:38 +0200

    fixed path and completion for gcloud

--- formulas/gcloud/completion.zsh
+++ formulas/gcloud/completion.zsh
@@ -1,9 +1,1 @@
-# The next line enables shell command completion for gcloud.
-if [ -f /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc ]; then
-  source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc'
-fi
-
-# Deprecated
-if [ -f /usr/local/lib/gcloud/google-cloud-sdk/completion.zsh.inc ]; then
-  source '/usr/local/lib/gcloud/google-cloud-sdk/completion.zsh.inc'
-fi
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
--- formulas/gcloud/path.zsh
+++ formulas/gcloud/path.zsh
@@ -1,9 +1,1 @@
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.in ]; then
-  source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.in'
-fi
-
-# Deprecated
-if [ -f /usr/local/lib/gcloud/google-cloud-sdk/path.zsh.inc ]; then
-  source '/usr/local/lib/gcloud/google-cloud-sdk/path.zsh.inc'
-fi
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
