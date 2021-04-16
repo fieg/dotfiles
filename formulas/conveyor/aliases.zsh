c68dbb40b741ed071b84166ff2b20a3af16311c4
fieg <j.fiege@geophy.com>
2021-04-16 13:17:32 +0200

    removed conveyor

--- formulas/conveyor/aliases.zsh
+++
@@ -1,1 +0,0 @@
-alias conveyor='~/git/conveyor/bin/conveyor'
--- formulas/conveyor/install.sh
+++
@@ -1,5 +0,0 @@
-test -d ~/git/conveyor || {
-    git clone git@github.com:webcreate/conveyor.git ~/git/conveyor
-    cd ~/git/conveyor
-    composer install
-}
