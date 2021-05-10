0488471063924b09622cbbcc5e1020cd20efb0ef
fieg <jeroen@fieg.nl>
2021-05-10 19:46:18 +0200

    added gmain

--- formulas/git/aliases.zsh
+++ formulas/git/aliases.zsh
@@ -1,2 +1,3 @@
 alias gfa='git fetch --all'
 alias gmaster='git stash && git fetch origin && git checkout -B master origin/master --track && git stash pop'
+alias gmain='git stash && git fetch origin && git checkout -B master origin/main --track && git stash pop'
