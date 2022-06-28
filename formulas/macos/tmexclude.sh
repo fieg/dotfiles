cca475a7cab8937155852b5dede7f564044ea95f
fieg <jeroen@fieg.nl>
2022-06-28 15:38:02 +0200

    ignore mode dirs

--- formulas/macos/tmexclude.sh
+++ formulas/macos/tmexclude.sh
@@ -5,14 +5,20 @@
 
 # Usage: tmexclude.sh ~/Path/to/code
 
-if [ $# -eq 0 ]; then
+if [ $# -lt 1 ]; then
     echo "Usage: tmexclude.sh ~/Path/to/code"
     exit
 fi
 
+CODE_PATH=$1
+
 # Always exclude downloads folder
-tmutil addexclusion ~/Downloads
-tmutil isexcluded ~/Downloads
+EXCLUDED_DIRECTORIES=( "~/Downloads" "~/.composer" "~/.npm/" )
+
+for EXCLUDED_DIRECTORY in "${EXCLUDED_DIRECTORIES[@]}"; do
+  tmutil addexclusion ${EXCLUDED_DIRECTORY}
+  tmutil isexcluded ${EXCLUDED_DIRECTORY}
+done
 
 # Ignore vendor and node_modules folders
-find $1 -maxdepth 8 -type d -name node_modules -prune -exec tmutil addexclusion {} \;  -exec tmutil isexcluded {} \; -o -name vendor -prune -exec tmutil addexclusion {} \; -exec tmutil isexcluded {} \;
+find $CODE_PATH -maxdepth 8 -type d -name node_modules -prune -exec tmutil addexclusion {} \;  -exec tmutil isexcluded {} \; -o -name vendor -prune -exec tmutil addexclusion {} \; -exec tmutil isexcluded {} \;
