53003cd7318cb7bc66015d2b7e6e0cf48a8874b0
fieg <jeroen@webcreate.nl>
2023-12-20 11:52:30 +0100

    added retry function

--- formulas/zsh/aliases.zsh
+++ formulas/zsh/aliases.zsh
@@ -11,3 +11,29 @@ fi
 
 # Make zsh know about hosts already accessed by SSH
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
+
+errcho() { >&2 echo $@; }
+
+# Retries a command on failure.
+# $1 - the max number of attempts
+# $2... - the command to run
+retry() {
+    local -r -i max_attempts="$1"; shift
+    local -r cmd="$@"
+    local -i attempt_num=1
+    local -i sleep_num=1
+
+    until eval ${cmd}
+    do
+        if (( attempt_num > max_attempts ))
+        then
+            return 1
+        else
+            local -i wait=$(( (attempt_num++)*sleep_num ))
+            errcho "[$(date '+%H:%M:%S')] Retry $(( attempt_num-1 ))/${max_attempts} in ${wait}s..."
+            sleep ${wait}
+        fi
+    done
+
+    return $?
+}
