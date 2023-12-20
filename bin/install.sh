61b1ff39d13dd85e5d2d56b987d06d84d148a2da
fieg <jeroen@webcreate.nl>
2023-12-20 16:35:34 +0100

    fixed "command not found: brew"

--- bin/install.sh
+++ bin/install.sh
@@ -43,6 +43,8 @@ setup_gitconfig () {
 install_brew () {
   info 'install homebrew'
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 }
 
 install_dotfiles () {
