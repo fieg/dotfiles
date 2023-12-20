9b0b1836653f0d785fefecb025efe85bc547413d
fieg <jeroen@webcreate.nl>
2023-12-20 16:18:34 +0100

    fail sudo

--- bin/install.sh
+++ bin/install.sh
@@ -98,7 +98,7 @@ success 'developer tools'
 
 # Ask for the administrator password upfront
 user "I need you to enter your sudo password so I can install some things:"
-sudo -v
+sudo -v || fail 'sudo required'
 
 # Keep-alive: update existing sudo time stamp until the script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
