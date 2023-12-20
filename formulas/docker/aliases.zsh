8d45c47fdbc2983c07ae1fa2a0f59e9ab776b204
fieg <jeroen@webcreate.nl>
2023-12-20 11:52:20 +0100

    added drmi function to remove docker images by name

--- formulas/docker/aliases.zsh
+++ formulas/docker/aliases.zsh
@@ -1,3 +1,8 @@
 alias d='docker'
 alias dc='docker compose'
 alias dkillall='docker kill $(docker ps -q)'
+
+# Remove docker images by name
+function drmi() {
+  docker rmi -f $(d images | grep $1 | awk '{print $3}')
+}
