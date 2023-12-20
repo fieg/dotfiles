8ff8b112fac5048d10a560bdc884183cbe011a33
fieg <jeroen@webcreate.nl>
2023-12-20 16:04:44 +0100

    set correct shell

--- bin/install.sh
+++ bin/install.sh
@@ -115,10 +115,10 @@ popd > /dev/null 2>&1
 # set zsh as the user login shell
 CURRENTSHELL=$(dscl . -read /Users/$USER UserShell | awk '{print $2}')
 if [[ "$CURRENTSHELL" != "/usr/local/bin/zsh" && -e /usr/local/bin/zsh ]]; then
-  info "setting newer homebrew zsh (/usr/local/bin/zsh) as your shell"
+  info "setting newer homebrew zsh (/opt/homebrew/bin/zsh) as your shell"
   # sudo bash -c 'echo "/usr/local/bin/zsh" >> /etc/shells'
   # chsh -s /usr/local/bin/zsh
-  sudo dscl . -change /Users/$USER UserShell $SHELL /usr/local/bin/zsh > /dev/null 2>&1
+  sudo dscl . -change /Users/$USER UserShell $SHELL /opt/homebrew/bin/zsh > /dev/null 2>&1
 fi
 success 'shell changed'
 
