4035240cebd53807846cbe1dbf0034da340385b8
fieg <jeroen@webcreate.nl>
2024-01-23 12:22:29 +0100

    greset and grebase with automatic master branch detection

--- formulas/git/aliases.zsh
+++ formulas/git/aliases.zsh
@@ -1,3 +1,61 @@
 alias gfa='git fetch --all'
-alias gmaster='git stash && git fetch origin && git checkout -B master origin/master --track && git stash pop'
-alias gmain='git stash && git fetch origin && git checkout -B master origin/main --track && git stash pop'
+alias gmaster='greset master'
+alias gmain='greset main'
+
+__detectMainBranch() {
+  if [ -n "$1" ]
+  then
+    echo "$1"
+    return 0
+  fi
+
+  >&2 echo -n "Automatically detecting HEAD branch..."
+  branch=$(git remote show origin 2> /dev/null | grep 'HEAD branch' | cut -d' ' -f 5)
+
+  >&2 echo " $branch"
+
+  if [ "$branch" = "master" ] || [ "$branch" = "main" ] || [ "$branch" = "trunk" ]
+  then
+    echo "$branch"
+  else
+    echo ""
+  fi
+}
+
+greset() {
+  branch=$(__detectMainBranch "$1")
+
+  if [ -z "$branch" ]
+  then
+    echo "Usage: $0 [branch]"
+    echo "Example: $0 main"
+    echo "Example: $0 # will attempt to fallback on HEAD branch"
+    return 1
+  fi
+
+  git fetch origin
+  git stash
+  git checkout -B $branch origin/$branch --track
+  git stash pop
+}
+
+grebase() {
+  if [ "$1" = "--abort" ] || [ "$1" = "--continue" ]
+  then
+    git rebase $1
+    return 0
+  fi
+
+  branch=$(__detectMainBranch "$1")
+
+  if [ -z "$branch" ]
+  then
+    echo "Usage: $0 [branch]"
+    echo "Example: $0 main"
+    echo "Example: $0 # will attempt to fallback on HEAD branch"
+    return 1
+  fi
+
+  git fetch origin
+  git rebase origin/$branch --autostash
+}
