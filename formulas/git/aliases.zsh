9847129712a985fd72da42e8e134ef2db50f165d
Jeroen Fiege <j.fiege@inshared.nl>
2026-03-27 11:19:08 +0000

    greset and grebase improvements

--- formulas/git/aliases.zsh
+++ formulas/git/aliases.zsh
@@ -23,26 +23,51 @@ __detectMainBranch() {
 }
 
 greset() {
+  zmodload zsh/zutil
+  zparseopts -D -E -F - h=help_flag -help=help_flag B:=local_branch -branch:=local_branch || return 1
+  local_branch=${local_branch[-1]} # turn "-B <branch_name>" into "<branch_name>"
+  local usage_text
+  usage_text=$(
+    cat <<'EOF'
+Usage: greset [-B local_branch] [branch]
+Example: greset main
+Example: greset # will attempt to fallback on HEAD branch
+EOF
+  )
+
+  if [ -n "$help_flag" ]
+  then
+    echo "$usage_text"
+    return 0
+  fi
+
   branch=$(__detectMainBranch "$1")
+
+  if [ -z "$local_branch" ]
+  then
+    local_branch=$branch
+  fi
 
   if [ -z "$branch" ]
   then
-    echo "Usage: $0 [branch]"
-    echo "Example: $0 main"
-    echo "Example: $0 # will attempt to fallback on HEAD branch"
+    echo "$usage_text"
     return 1
   fi
 
   git fetch origin
   git stash
   git checkout -B $branch origin/$branch --track
+  git checkout -b $local_branch HEAD --
   git stash pop
 }
 
 grebase() {
-  if [ "$1" = "--abort" ] || [ "$1" = "--continue" ]
+  zmodload zsh/zutil
+  zparseopts -D -E -F - -abort=rebase_flags -continue=rebase_flags -push=push_flag || return 1
+
+  if [ -n "$rebase_flags" ]
   then
-    git rebase $1
+    git rebase $rebase_flags
     return 0
   fi
 
@@ -50,12 +75,20 @@ grebase() {
 
   if [ -z "$branch" ]
   then
-    echo "Usage: $0 [branch]"
+    echo "Usage: $0 [branch] [--push] [--abort] [--continue]"
     echo "Example: $0 main"
     echo "Example: $0 # will attempt to fallback on HEAD branch"
+    echo "Example: $0 --abort # aborts the rebase"
+    echo "Example: $0 --continue # continues the rebase"
+    echo "Example: $0 --push # fallback on HEAD and force push after rebase"
     return 1
   fi
 
   git fetch origin
   git rebase origin/$branch --autostash
+
+  if [ -n "$push_flag" ]
+  then
+    git push origin --force
+  fi
 }
